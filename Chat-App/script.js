d506f02d0380a201e0bc62af2c72115131a00bdb
Nawal <nr23abp@herts.ac.uk>
2024-12-02 14:30:49 +0000

    set up

---
+++ Chat-App/script.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'firebase-auth-hooks/auth';
+import { useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    )
+}
